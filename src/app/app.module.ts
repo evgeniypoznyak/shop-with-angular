aa012658107cf2bc2f51ce6dc8785e431e6ee199
Evgeniy Poznyak <ek35mm@gmail.com>
2017-08-04 17:22:15 -0500

    246 - Store data in Firebase - Get data from Firebase - If not ingredients array, add empty - Save & Fetch data buttons implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
+import { DropDownDirective } from './shared/dropdown.selected';
+
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
+import { DataStorageService } from './shared/data-storage.service';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,11 +18,9 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropDownDirective } from './shared/dropdown.selected';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeService } from './recipes/recipe.service';
+
 
 
 @NgModule({
@@ -36,9 +41,10 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [ShoppingListService, RecipeService],
+  providers: [ShoppingListService, RecipeService, DataStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +11,17 @@ import { Component } from '@angular/core';
 
 export class HeaderComponent {
 
+  constructor(private dataStorageService: DataStorageService) {
+  }
 
+  onSaveData() {
+    this.dataStorageService.storeRecipes().subscribe(
+      (response: Response) => {
+        console.log(response.json());
+      }
+    );
+  }
+  onFetchData(){
+    this.dataStorageService.getRecipes();
+  }
 }
