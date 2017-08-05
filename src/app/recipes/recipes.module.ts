b21a260632c47eb6206a7ec99c6e9a1cf21f91ec
Evgeniy Poznyak <ek35mm@gmail.com>
2017-08-05 12:18:57 -0500

    267 - Split App into modules - Add RecipesModule - Add RecipesRoutingModule

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
+
+@NgModule({
+  declarations: [
+    // defining types and in import (bellow) we tell TypeScript where to find this classes
+    RecipesComponent,
+    RecipeStartComponent,
+    RecipeListComponent,
+    RecipeEditComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
+  ],
+  imports: [
+    // CommonModule need for common features, and it has to be in any additional modules
+    CommonModule,
+    ReactiveFormsModule,
+    RecipesRoutingModule
+  ]
+})
+export class RecipesModule {
+
+}
